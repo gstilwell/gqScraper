938c87a54f378f233d8593fa731496f8b843ab80
Geoff Stilwell <gstilwell@gmail.com>
2021-04-10 21:35:41 -0400

    started adding database support

---
+++ GQDB.py
@@ -0,0 +1,59 @@
+import psycopg2
+from datetime import datetime
+
+class GQDB:
+    def __init__(self, name, host, port, username, password):
+        dbConnectString = "dbname='{dbname}' host='{host}' port='{port}' user='{user}' password='{password}'"\
+            .format(dbname = name, host = host, port = port, user = username, password = password)
+        try:
+            self.db = psycopg2.connect(dbConnectString)
+            self.cursor = self.db.cursor()
+        except Exception as e:
+            print("couldn't connect to db")
+            print(e)
+
+        self.classicSchema = "main"
+        self.scrapeSchema = "gqscrape"
+
+    def query_write_only(self, query):
+        self.cursor.execute(query)
+        self.db.commit()
+
+    def query_one(self, query):
+        self.cursor.execute(query)
+        self.db.commit()
+        row = self.cursor.fetchone()
+        return row
+
+    def query(self, query):
+        self.cursor.execute(query)
+        self.db.commit()
+        row = self.cursor.fetchall()
+        return row
+
+    def most_recent_question_id(self):
+        query = """SELECT id FROM {schema}.question ORDER BY id DESC LIMIT 1""".format(schema = self.scrapeSchema)
+        row = self.query_one(query)
+        return row[0]
+
+    def is_user(self, schema, username):
+        query = """SELECT * FROM {schema}.user WHERE user = '{username}'""".format(schema = schema, user = username)
+        row = self.query_one(query)
+        return true if row else false
+
+    def is_classic_user(self, username):
+        self.is_user(self.classicSchema, username)
+
+    def is_new_user(self, username):
+        self.is_user(self.scrapeSchema, username)
+
+    def write_question(self, qDict):
+        query = """INSERT INTO {schema}.question(id, text, thumbs, gold, user_id, date, scrape_time, num_answers)\
+            VALUES ({id}, '{text}', NULL, NULL, '{user_id}', '{date}', '{scrape_time}', NULL)"""\
+            .format(schema = self.scrapeSchema, id = qDict["id"], text = qDict["text"],\
+            user_id = qDict["user_id"], date = qDict["date"], scrape_time = datetime.now().__str__().replace(' ', 'T'))
+        self.query_write_only(query)
+
+    def __del__(self):
+        self.cursor.close()
+        self.db.close()
--- gqScraper.py
+++ gqScraper.py
@@ -4,28 +4,49 @@ from time import sleep
 import os.path
 
 from BGGElementScraper import BGGElementScraper
+from GQDB import GQDB
 
 parser = argparse.ArgumentParser(description='Scrub GQ')
-parser.add_argument('username', type=str, help='BGG username')
-parser.add_argument('password', type=str, help='BGG user password')
+parser.add_argument('bgg_username', type=str, help='BGG username')
+parser.add_argument('bgg_password', type=str, help='BGG user password')
+parser.add_argument('db_name', type=str, help='db name')
+parser.add_argument('db_host', type=str, help='db hostname')
+parser.add_argument('db_port', type=str, help='db port')
+parser.add_argument('db_username', type=str, help='db username')
+parser.add_argument('db_password', type=str, help='db password')
 args = parser.parse_args()
 
-scraper = BGGElementScraper(args.username, args.password)
+#scraper = BGGElementScraper(args.bgg_username, args.bgg_password)
+db = GQDB(args.db_name, args.db_host, args.db_port, args.db_username, args.db_password)
 
-usersFile = open('users.json', 'r')
-usersObj = json.load(usersFile)
-usersFile.close()
+question = {
+    "id": 211919,
+    "text": "test question?",
+    "user_id": 103374,
+    "date": "2005-04-28 15:06:26"
+}
+db.write_question(question)
 
-avatarDir = "avatars"
-usernames = usersObj['rows']
-usernames = [user[0] for user in usernames]
+id = db.most_recent_question_id()
+print(id)
 
-for username in usernames:
-    validUsername = username.replace(":", "@@@COLON@@@").replace("*", "@@@ASTERISK@@@")
-    noAvatarPath = os.path.join(avatarDir, validUsername + ".noavatar")
-    avatarPath = os.path.join(avatarDir, validUsername + ".jpg")
-    if os.path.exists(noAvatarPath) or os.path.exists(avatarPath):
-        continue
+#usersFile = open('users.json', 'r')
+#usersObj = json.load(usersFile)
+#usersFile.close()
+#
+#avatarDir = "avatars"
+#usernames = usersObj['rows']
+#usernames = [user[0] for user in usernames]
+#
+#while True:
 
-    scraper.saveAvatar(username, avatarDir)
-    sleep(5)
+
+#for username in usernames:
+#    validUsername = username.replace(":", "@@@COLON@@@").replace("*", "@@@ASTERISK@@@")
+#    noAvatarPath = os.path.join(avatarDir, validUsername + ".noavatar")
+#    avatarPath = os.path.join(avatarDir, validUsername + ".jpg")
+#    if os.path.exists(noAvatarPath) or os.path.exists(avatarPath):
+#        continue
+#
+#    scraper.saveAvatar(username, avatarDir)
+#    sleep(5)
